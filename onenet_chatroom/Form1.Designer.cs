a6d4aacc4f4a66466f8f0acebe97f3533f2a6668
李茂 <limao@iot.chinamobile.com>
2015-06-04 08:45:35 +0800

    upload V1 Alpha

---
+++ onenet_chatroom/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace onenet_chatroom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tb1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(675, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(612, 482);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(41, 12);
+            this.lb1.TabIndex = 1;
+            this.lb1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 436);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // tb1
+            // 
+            this.tb1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb1.Location = new System.Drawing.Point(275, 53);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(475, 394);
+            this.tb1.TabIndex = 3;
+            this.tb1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(675, 477);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(275, 455);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(378, 21);
+            this.tb2.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 555555;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 463);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "内容：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 506);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "基于onenet的在线聊天室";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox tb1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ onenet_chatroom/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+
+namespace onenet_chatroom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   //     string ip = Convert.ToString((IPAddress)Dns.GetHostAddresses(Dns.GetHostName()).GetValue(0));
+        string ip = "";
+        string url_chat_log = "http://api.heclouds.com/devices/你的设备ID/datapoints?datastream_id=chatlog";
+        string url_chat_update = "http://api.heclouds.com/devices/你的设备ID/datapoints";
+        string url_chat_online_people_get = "http://api.heclouds.com/devices/你的设备ID/datapoints?datastream_id=online1";
+        string url_chat_online_people_post = "http://api.heclouds.com/devices/你的设备ID/datapoints";
+
+
+        public string WebRequest(string method, string url, string postData)
+        {
+            HttpWebRequest webRequest = null;
+            StreamWriter requestWriter = null;
+            string responseData = "";
+
+            webRequest = System.Net.WebRequest.Create(url) as HttpWebRequest;
+            webRequest.Method = method.ToString();
+            webRequest.ServicePoint.Expect100Continue = false;
+            webRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36";
+
+                webRequest.Timeout = 5000;
+
+
+                webRequest.Headers.Add(tb_header_ext);
+            
+
+            if (method == "POST" || method == "PUT")
+            {
+                if (method == "PUT")
+                {
+                    webRequest.ContentType = "text/xml";
+                    webRequest.Method = "PUT";
+                }
+                else
+                    webRequest.ContentType = "application/x-www-form-urlencoded";
+                //webRequest.ContentType = "multipart/form-data";
+
+                //POST the data.
+                requestWriter = new StreamWriter(webRequest.GetRequestStream());
+                try
+                {
+                    requestWriter.Write(postData);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    requestWriter.Close();
+                    requestWriter = null;
+                }
+            }
+
+            responseData = WebResponseGet(webRequest);
+
+            webRequest = null;
+
+            return responseData;
+
+        }
+
+        public string WebResponseGet(HttpWebRequest webRequest)
+        {
+            StreamReader responseReader = null;
+            string responseData = "";
+
+            try
+            {
+                responseReader = new StreamReader(webRequest.GetResponse().GetResponseStream());
+                responseData = responseReader.ReadToEnd();
+
+
+                webRequest.GetResponse().GetResponseStream().Close();
+                responseReader.Close();
+                responseReader = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+
+            }
+
+            return responseData;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lb1.Text = "发送中……";
+
+            string post_text = "{\"datastreams\" : [{\"id\" : \"" + "chatlog" + "\",\"datapoints\" : [{"
+                    + "\"value\" : {"
+                    + "\"user\" : " + Environment.UserName + ","
+                    + "\"content\" : " + tb2.Text
+                    + "}}]}]}";
+
+
+            WebRequest("POST", url_chat_update, post_text);
+
+            tb2.Text = "";
+            lb1.Text = "就绪";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add(Environment.UserName + "-" + Environment.UserDomainName + "  " + ip);
+
+            tb1.SelectionColor = Color.Red;
+            tb1.AppendText("cs\r\n");
+
+            tb1.SelectionColor = Color.Blue;
+            tb1.AppendText("cs\r\n");
+
+           // lb1.Text = DateTime.Now.AddMinutes(-10)+"";
+            lb1.Text = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+
+        }
+
+        private void clear_room() {
+            //清空聊天室人数
+            string post_text = "{\"datastreams\" : [{\"id\" : \"" + "online1" + "\",\"datapoints\" : [{"
++ "\"value\" : {"
++ "\"people\" : " + "在线的人："
++ "}}]}]}";
+
+
+            WebRequest("POST", url_chat_online_people_post, post_text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lb1.Text = "就绪";
+
+            clear_room();
+            
+
+
+
+            Thread th_request;
+            
+            th_request = new Thread((ThreadStart)delegate()
+            {
+                do_th_request();
+            });
+            th_request.IsBackground = true;
+            th_request.Start();
+        
+
+          Thread th_request2;
+            
+            th_request2 = new Thread((ThreadStart)delegate()
+            {
+                do_th_request2();
+            });
+            th_request2.IsBackground = true;
+            th_request2.Start();
+        }
+
+        //聊天的人
+        private void do_th_request2() {
+            while (true)
+            {
+                this.getonliepeople();
+
+                Thread.Sleep(3333);
+            }
+        }
+
+        //聊天记录的获取
+        private void do_th_request()
+        {
+            string time = "";
+            while (true)
+            {
+                if (time == "") {
+                    string post_text = "{\"datastreams\" : [{\"id\" : \"" + "chatlog" + "\",\"datapoints\" : [{"
++ "\"value\" : {"
++ "\"user\" : " + Environment.UserName + ","
++ "\"content\" : " + Environment.UserName + "加入了聊天室"
++ "}}]}]}";
+                    WebRequest("POST", url_chat_update, post_text);
+                    Thread.Sleep(1234);
+                }
+             
+                    string res = WebRequest("GET", url_chat_log, "");
+
+                    //          JsonReader reader = new JsonTextReader(new StringReader(res));
+
+                    int at = res.IndexOf("datastreams\":[{\"datapoints\":[{\"at\":\"");
+                    int user = res.IndexOf("{\"user\":");
+                    int value = res.IndexOf(",\"content\":\"");
+                    int value_end = res.IndexOf("\"}}],\"id");
+
+                    string time_temp = time;
+
+                    time = res.Substring(at + 36, 22);
+
+                    string adduser = res.Substring(user + 9, value - user - 10);
+
+                    string content = res.Substring(value + 12, value_end - value - 12);
+
+                    Thread.Sleep(222);
+
+                    if (time_temp == time)
+                    {
+                        continue;
+                    }
+
+                    this.Invoke((MethodInvoker)delegate()
+                    {
+                        if (adduser == Environment.UserName)
+                        {
+                            tb1.SelectionColor = Color.Green;
+                        }
+                        else
+                        {
+                            tb1.SelectionColor = Color.Blue;
+                        }
+                        tb1.AppendText(adduser + " "); tb1.AppendText(time + " ");
+                        tb1.AppendText("\r\n");
+                        tb1.SelectionColor = Color.Black;
+                        tb1.AppendText(content + "\r\n\r\n");
+                        //   tb1.AppendText(res);
+                    });
+
+               // break;
+            }
+            
+        }
+
+
+        private void getonliepeople() {
+
+            string res = WebRequest("GET", url_chat_online_people_get, "");
+
+            int pos_start = res.IndexOf(",\"value\":{\"people\":\"");
+            int pos_end = res.IndexOf("\"}}],\"");
+
+            string people = res.Substring(pos_start + 20, pos_end - pos_start-20);
+
+            if (people.IndexOf(Environment.UserName + "|" + Environment.UserDomainName + "|" + ip) < 0)
+            {
+                people = people + "*" + Environment.UserName + "|" + Environment.UserDomainName + "|" + ip;
+                string post_text = "{\"datastreams\" : [{\"id\" : \"" + "online1" + "\",\"datapoints\" : [{"
++ "\"value\" : {"
++ "\"people\" : " + people
++ "}}]}]}";
+
+                WebRequest("POST", url_chat_online_people_post, post_text);
+            }
+            
+
+            this.Invoke((MethodInvoker)delegate()
+            {
+                string[] sArray = people.Split('*');
+                listBox1.Items.Clear();
+                foreach (string i in sArray) { listBox1.Items.Add(i); }
+        //        MessageBox.Show(people + "\r\n"+DateTime.Now + "");
+                //lb1.Text = people + DateTime.Now + "";
+            });
+            
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            clear_room();
+           // this.getonliepeople();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
